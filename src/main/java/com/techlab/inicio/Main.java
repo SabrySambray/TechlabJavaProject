fd67a64e5f3f54a2e5b64d84eb82a177a214a852
Sabryna Sambray <141812366+SabrySambray@users.noreply.github.com>
2025-05-25 19:16:59 -0300

    cambios

--- src/main/java/com/techlab/inicio/Main.java
+++ src/main/java/com/techlab/inicio/Main.java
@@ -1,17 +1,42 @@
 package com.techlab.inicio;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.techlab.productos.*;
+import com.techlab.pedidos.*;
+import com.techlab.servicios.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ProductoService productoService = new ProductoService();
+        PedidoService pedidoService = new PedidoService(productoService);
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        do {
+            System.out.println("\n============= SISTEMA DE GESTIÓN - TECHLAB =============");
+            System.out.println("1) Agregar producto");
+            System.out.println("2) Listar productos");
+            System.out.println("3) Buscar/Actualizar producto");
+            System.out.println("4) Eliminar producto");
+            System.out.println("5) Crear un pedido");
+            System.out.println("6) Listar pedidos");
+            System.out.println("7) Salir");
+            System.out.println("8) Agregar productos de ejemplo (recomendable para testear)");
+            System.out.print("Elija una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine(); // Limpiar buffer
+
+            switch (opcion) {
+                case 1 -> productoService.agregarProducto(scanner);
+                case 2 -> productoService.listarProductos();
+                case 3 -> productoService.buscarActualizarProducto(scanner);
+                case 4 -> productoService.eliminarProducto(scanner);
+                case 5 -> pedidoService.crearPedido(scanner);
+                case 6 -> pedidoService.listarPedidos();
+                case 7 -> System.out.println("Saliendo del sistema. ¡Hasta pronto!");
+                case 8 -> productoService.agregarProductosEjemplo();
+                default -> System.out.println("Opción inválida. Intente nuevamente.");
+            }
+        } while (opcion != 7);
+
+        scanner.close();
     }
 }
